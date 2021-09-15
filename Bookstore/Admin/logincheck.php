be4d797fdd86217756591f2483fbfb24c773ad3f
Md.  Abu Sayed Fahim <86910379+SayedFahim03@users.noreply.github.com>
2021-09-15 21:13:40 +0600

    Delete Bookstore directory

--- Bookstore/Admin/logincheck.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!isset($_SESSION['aloggedin'])) {
-  header('location: login.php');
-}
-
- ?>
--- Bookstore/User/logincheck.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!isset($_SESSION['loggedin'])) {
-  header('location: login.php');
-}
-
- ?>
